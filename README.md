271d58310c56238b5f8fe4029be24be5fc460766
DictatorBob <thatmanagain@gmail.com>
2018-05-06 00:52:16 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # crostini-pie
-Post-Install Expedited, for Crostini
+Post install expedited, for Crostini. 
+
+# What is this?
+This is a simple bash script for setting up a container from scratch, after initial installation. It runs through a series of modular steps (called "ingredients in the script, because pie, geddit?), each of which does a fairly simple thing, like setting the host name of the container, setting up the Debian stretch backports repositories, as well as the contrib and non-free ones, and then installs a bunch of software, mostly with apt, but some of it using GNU stow.
+
+**Important Disclaimer**
+It has only been tested on my own PixelBook. It works for me. You should review the script to make sure it doesn't eat all your ice cream, run away with your dog, sleep with your truck, or otherwise ruin your life.
+
+# Why would you use it?
+I found myself often destroying and reinstalling containers, whether for trying things out, because of bugs, or whatever other reason. So repeatedly rebuilding all the tools I need for my daily work quickly became tiresome and time-consuming. 
+My PixelBook is my daily driver and I need to be able to quickly restore from scratch and be up and running with a minimum of effort. 
+
+Also, I tend to use software that is newer than what's available in Debian stretch (or simply isn't available at all). 
+For my purposes, backports mostly does the job, but some things are built from source, or are simply binary downloads (like terraform). More details on what exactly the script builds below, but it's geared towards my own purposes, which is as a devops workstation. Should be easy to modify it for your own uses.
+
+
+# How would you use it?
+It's just a bash script. Since a new Crostini container doesn't have git, just download it with curl, and execute it (give it a read-through first).
+
+```
+curl -o crostini-pie.sh https://github.com/DictatorBob/crostini-pie/blob/master/crostini-pie.sh
+sh crostini-pie.sh
+```
+# What exactly does it do?
+First of all, for every step or function that it executes, it will prompt you first. If you don't want it to do a thing, simply reply to the prompt with a single lower-case "n". If you want it to do the thing, press Enter. You can also type in arguments for the function, if it needs any. For example, in the case of the terraform and packer installer, you can specify the version, and it will download/install exactly that.
+
+When you run the script, it will:
+1. Set the hostname for the container **guest**. It's not renaming the LXD container, which has to be named "penguin" for the Google-provided app shortcut to find it. It simply sets the hostname of your guest OS.
+2. It creates new apt sources in /etc/apt/sources.list.d to make available additional Debian repos (backports, contrib, non-free). If you're a Free Software advocate, you should edit those appropriately (but of course, the PixelBook and ChromeOS are already not technically free software).
+3. It adds a selection of software packages from the apt repos. In the shell script, these packages are defined in two lists: "required" and "optional". Those names are pretty self-explanatory. You won't be prompted for the stuff in the required list, but for every package listed in "optional" you'll have the opportunity to hit Enter to add it or "n" to skip it. It should be trivial to edit the script and add your own choices to the list
+4. It installs termite from source. This happens to be my favourite terminal emulator. Skip it if you prefer another. The install script is more or less verbatim from here: https://github.com/Corwind/termite-install/blob/master/termite-install.sh except with slight modifications to use stow. **Warning: Adds a lot of packages**
+5. It installs emacs from source. This is a lengthy process, so skip if you're not an Emacs user. **Warning: Lengthy compile time, adds a lot of packages**
+6. It installs terraform and packer (binary downloads) and ansible (from the official ansible ppa)
+
+# Additional notes
+
+ - It's meant to be idempotent, so in theory you can run and re-run the script as many times as you like
+ - It should be trivially simple to add an ingredient. Just add the name of your ingredient to the list at the beginning. This name can be anything. Then add a "case" command under the function "add_ingredient"
+
---
+++ crostini-pie.sh
@@ -0,0 +1,171 @@
+#!/usr/bin/env bash
+
+required="build-essential pkg-config git stow"
+optional="gnupg tmux rsync mosh"
+
+
+ingredients="sethostname \
+        setrepos \
+        backports \
+        termite \
+        ansible \
+        terraform \
+        packer \
+        emacs \
+"
+
+function add_ingredient {
+    case $1 in
+        sethostname)
+            # Set hostname: The container needs to be called penguin, at least if you want to use the app shortcut
+            # But you can rename the host.
+            # You may get a complaint from sudo saying "unable to resolve host <whatever old name>". Safe to ignore it
+            read -p "## Enter hostname for this container: " newname ; \
+            echo "## It's safe to ignore the following sudo warning"
+            sudo sed -i "s/127\.0\.1\.1.*/127\.0\.1\.1\t${newname}/" /etc/hosts
+            sudo hostnamectl set-hostname ${newname} ; \
+            ;;
+
+        setrepos)
+            # Add contrib, non-free Debian repos, as well as backports
+            # These are in a separate file in /etc/apt/sources.lists.d for easy removal if desired
+            sudo sh -c 'cat > /etc/apt/sources.list.d/crostini-pie.list' << EOF
+
+            # Debian contrib and non-free repos as well as stretch-updates
+            deb http://deb.debian.org/debian stretch contrib non-free
+            deb http://deb.debian.org/debian stretch-updates main contrib non-free
+            deb http://security.debian.org/debian-security/ stretch/updates contrib non-free
+            deb http://ftp.debian.org/debian stretch-backports main contrib non-free
+EOF
+            sudo apt update
+            ;;
+
+        backports)
+            # Steps through a list of packages to install, all from backports by default
+
+            final=""
+            echo "## Hit Enter to add each package to the list to be installed, or n to skip it"
+            for pkg in ${optional}; do
+                    read -p "#### Install ${pkg}? (Y/n)" yorn
+                    case ${yorn} in
+                        n) 
+                            echo "#### Skipping ${pkg}"
+                            ;;
+                        *)
+                            echo "#### Adding ${pkg} to the install list"
+                            final="${final} ${pkg}"
+                            ;;
+                    esac
+            done
+
+            echo "##### Final list of packages:"
+            echo "${final}"
+            sudo apt-get -y -t stretch-backports install ${required} ${final}
+            ;;
+
+        termite)
+            # from https://github.com/Corwind/termite-install/blob/master/termite-install.sh
+
+            sudo apt-get install -y \
+                git \
+                g++ \
+                libgtk-3-dev \
+                gtk-doc-tools \
+                gnutls-bin \
+                valac \
+                intltool \
+                libpcre2-dev \
+                libglib3.0-cil-dev \
+                libgnutls28-dev \
+                libgirepository1.0-dev \
+                libxml2-utils \
+                gperf
+                
+            git clone --recursive https://github.com/thestinger/termite.git
+            git clone https://github.com/thestinger/vte-ng.git
+            
+            echo export LIBRARY_PATH="/usr/include/gtk-3.0:$LIBRARY_PATH"
+            cd vte-ng && ./autogen.sh && make && sudo make install
+            cd ../termite && make && sudo make install
+            sudo ldconfig
+            sudo mkdir -p /lib/terminfo/x; sudo ln -s \
+            /usr/local/share/terminfo/x/xterm-termite \
+            /lib/terminfo/x/xterm-termite
+            
+            sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/termite 60
+            ;;
+            
+        ansible)
+            #Source: http://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-apt-debian
+            # When you run the next command you'll get an error about the gpg key for the ansible ppa
+            sudo sh -c 'cat > /etc/apt/sources.list.d/ansible.list' << EOF
+            deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main
+EOF
+            sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+            sudo apt-get update
+            sudo apt-get -y install ansible
+            ;;
+        terraform)
+            TFVersion="${choice:-0.11.7}"
+            echo "#### Default terraform version is ${TFVersion}"
+            curl -o terraform_${TFVersion}_linux_amd64.zip https://releases.hashicorp.com/terraform/${TFVersion}/terraform_${TFVersion}_linux_amd64.zip
+            unzip terraform_${TFVersion}_linux_amd64.zip
+            rm terraform_${TFVersion}_linux_amd64.zip
+            sudo mkdir -p /usr/local/stow/terraform
+            sudo mv terraform /usr/local/stow/terraform/terraform-${TFVersion}
+            cd /usr/local/stow/terraform
+            sudo ln -s terraform-${TFVersion} terraform
+            cd ..
+            sudo stow -t /usr/local/bin terraform
+            cd
+            ;;
+
+        packer)
+            PKVersion="${choice:-1.2.3}"
+            echo "#### Default packer version is ${PKVersion}"
+            curl -o packer_${PKVersion}_linux_amd64.zip https://releases.hashicorp.com/packer/${PKVersion}/packer_${PKVersion}_linux_amd64.zip
+            unzip packer_${PKVersion}_linux_amd64.zip
+            rm packer_${PKVersion}_linux_amd64.zip
+            sudo mkdir -p /usr/local/stow/packer
+            sudo mv packer /usr/local/stow/packer/packer-${PKVersion}
+            cd /usr/local/stow/packer
+            sudo ln -s packer-${PKVersion} packer
+            cd ..
+            sudo stow -t /usr/local/bin packer
+            cd
+            ;;
+        emacs)
+            git clone -b master git://git.sv.gnu.org/emacs.git
+            sudo apt-get -y install build-essential automake texinfo libjpeg-dev libncurses5-dev
+            sudo apt-get -y install libtiff5-dev libgif-dev libpng-dev libxpm-dev libgtk-3-dev libgnutls28-dev 
+            cd emacs/
+            ./autogen.sh 
+            ./configure --with-mailutils --prefix=/usr/local/stow/emacs
+            make
+            sudo make install
+            cd /usr/local/stow
+            sudo stow emacs
+            ;;
+        *)  echo "That's all"
+            ;;
+    esac
+}
+
+function doyouwant {
+    echo ""
+    echo "## About to execute function: ${i}"
+    echo "## Enter to proceed, n to skip this ingredient, or any other input will be treated as parameters for the function"
+    read -e -p "## Install $1? (Y/n/args) " choice
+    echo ""
+    case ${choice} in
+        n|N)    echo "## Skipping $1"
+            ;;
+        *)  echo "## Executing $1" 
+            add_ingredient $1 $choice
+            ;;
+    esac
+}
+
+for i in ${ingredients}; do
+    doyouwant $i
+done
